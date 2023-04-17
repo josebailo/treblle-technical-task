3ce2dc46dd7c26dc828b723b4a19fee0d738c12f
Jose Bailo <jmbailo@gmail.com>
2023-04-17 22:57:48 +0100

    Install TailwindCSS and Standard JS

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true
+    })
+  ],
+  // Configuration needed to work on WSL2
+  server: {
+    hmr: {
+      host: 'localhost'
+    },
+    watch: {
+      usePolling: true
+    }
+  }
+})
