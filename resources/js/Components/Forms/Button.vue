b54b12580c5e9e876cd589fa412d8110f22c6879
Jose Bailo <jmbailo@gmail.com>
2023-04-25 23:54:52 +0100

    Add hover effect to Button component

--- resources/js/Components/Forms/Button.vue
+++ resources/js/Components/Forms/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="px-4 py-2 rounded-md bg-blue-600 text-white shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 focus:ring-offset-2">
+  <button class="px-4 py-2 rounded-md bg-blue-600 text-white shadow-sm hover:bg-blue-500 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 focus:ring-offset-2">
     <slot />
   </button>
 </template>
